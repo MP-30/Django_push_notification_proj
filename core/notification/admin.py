51e1185fa156aa6f502a6d1fab8de00c140d027d
adityamoney <adityabhadauriya93@gmail.com>
2024-03-10 23:49:52 +0530

    notification provision in template

--- core/notification/admin.py
+++ core/notification/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from django import forms
+from .models import Notification
+
+class SecondNotificationForm(forms.Form):
+    message = forms.CharField(label = "Notification Mesasge", max_length=200)
+    
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    pass
