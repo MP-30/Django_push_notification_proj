054af82d8b38bca9c88bb2121111b814c7da74c6
MP-30 <adityabhadauriya93@gmail.com>
2024-03-22 23:20:44 +0530

    some work on notification

--- core/notification/admin.py
+++ core/notification/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from django import forms
+from django.http import HttpRequest
+from django.http.response import HttpResponse
 from .models import Notification
 
 class SecondNotificationForm(forms.Form):
@@ -9,3 +11,18 @@ class SecondNotificationForm(forms.Form):
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     add_form_template = "admin/custon_add_form.html"
+    
+    def add_view(self, request, form_url="", extra_context= None):
+        if request.method == "POST":
+            form = SecondNotificationForm(request.POST)
+            if form.is_valid():
+                message = form.cleaned_data["message"]
+                
+                notification = Notification.objects.create(message = message)
+                
+            else:
+                form = SecondNotificationForm()
+                
+            context = self.get_changeform_initial_data(request)
+            context["form"] = form
+            return super().add_view(request, form_url, extra_context=context)
