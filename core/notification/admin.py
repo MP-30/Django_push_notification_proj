5a25818a83c219d5cfa3e3a4e03e88ce3e88ca1a
MP-30 <adityabhadauriya93@gmail.com>
2024-03-27 22:22:28 +0530

    admin part of notificatn done

--- core/notification/admin.py
+++ core/notification/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 from django import forms
 from django.http import HttpRequest
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import path
 from .models import Notification
 
-class SecondNotificationForm(forms.Form):
+class SendNotificationForm(forms.Form):
     message = forms.CharField(label = "Notification Mesasge", max_length=200)
     
 @admin.register(Notification)
@@ -14,15 +17,33 @@ class NotificationAdmin(admin.ModelAdmin):
     
     def add_view(self, request, form_url="", extra_context= None):
         if request.method == "POST":
-            form = SecondNotificationForm(request.POST)
+            form = SendNotificationForm(request.POST)
             if form.is_valid():
                 message = form.cleaned_data["message"]
                 
                 notification = Notification.objects.create(message = message)
                 
+                channel_layer = get_channel_layer()
+                async_to_sync(channel_layer.group_send)(
+                    "notifications",
+                    {
+                        "type": "send_notification",
+                        "message": message
+                    }
+                )
+                
+                return HttpResponseRedirect("../{}/".format(notification.pk))
+                
         else:
-            form = SecondNotificationForm()
+            form = SendNotificationForm()
             
         context = self.get_changeform_initial_data(request)
         context["form"] = form
         return super().add_view(request, form_url, extra_context=context)
+    
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_url = [
+            path("send-notification/", self.admin_site.admin_view(self.add_view), name="send-notification")
+        ]
+        return custom_url + urls
